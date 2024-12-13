f0d2af4d78c02ff6e473c462a2f6111cc98d9708
sba <s.blanco@kolabnow.com>
2024-12-12 21:57:43 -0500

    temporary deck storage

---
+++ src/Deck.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+std::array<std::string,10> Player1DeckFile{ // TODO : Use proper storage of deck
+        "../assets/img/cards/player1/bigJump.png",
+        "../assets/img/cards/player1/bigKnight.png",
+        "../assets/img/cards/player1/center.png",
+        "../assets/img/cards/player1/corner.png",
+        "../assets/img/cards/player1/diagonal.png",
+        "../assets/img/cards/player1/free.png",
+        "../assets/img/cards/player1/jump.png",
+        "../assets/img/cards/player1/knight.png",
+        "../assets/img/cards/player1/neighbour.png",
+        "../assets/img/cards/player1/side.png"
+    };
+
+    std::array<std::string,10> Player2DeckFile{ // TODO : Use proper storage of deck
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+        "../assets/img/cards/player2/free.png",
+    };
+
+    std::array<int,10> Player1DeckID{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std::array<int,10> Player2DeckID{ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
