9bdbc1ed7988198d9e391fa6c533a43532845760
sba <s.blanco@kolabnow.com>
2025-01-15 08:43:02 -0500

    add pass button layout

--- src/Layout.h
+++ src/Layout.h
@@ -22,7 +22,7 @@ inline const int BigCardWidth{300};
 inline const int BigCardHeight{static_cast<int>(1.44*BigCardWidth)};
 
 inline const int CardMarginX{(ScreenWidth - GobanTopX - GobanWidth - 2*CardWidth)/3};
-inline const int CardMarginY{(GobanHeight - 2*CardHeight)/4};
+inline const int CardMarginY{0};
 
 inline const int BigModeX{GobanTopX + GobanWidth + (ScreenWidth - GobanTopX - GobanWidth - BigCardWidth)/2};
 inline const int BigModeY{GobanTopY + (GobanHeight - BigCardHeight)/2};
@@ -33,5 +33,7 @@ inline const int TopHandY{GobanTopY + CardMarginY};
 inline const int TopDeckX{CardMarginX};
 inline const int TopDeckY{GobanTopY + GobanHeight - CardHeight};
 
-inline const int InterRowY{(GobanHeight - 2*(CardMarginY + CardHeight))/2};
+inline const int PassButtonHeight{75};
+inline const int PassButtonMarginY{10};
+inline const int InterRowY{(GobanHeight - 2*CardHeight - CardMarginY - PassButtonHeight + PassButtonMarginY)/2};
 }
